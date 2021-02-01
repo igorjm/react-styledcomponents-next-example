a1a4284f64ce8f0298d4b9c4909209edc8dc7f39
Igor Melo <igorjmelo4@gmail.com>
2021-02-01 14:38:09 -0300

    new components | quiz page

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,21 @@
-import React from "react";
-import styled from "styled-components";
-import Head from "next/head";
-import { useRouter } from "next/router";
+import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-import db from "../db.json";
+import db from '../db.json';
 
-import Widget from "../src/components/Widget";
-import QuizLogo from "../src/components/QuizLogo";
-import QuizBackground from "../src/components/QuizBackground";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
-
-export const QuizContainer = styled.div`
-    width: 100%;
-    max-width: 350px;
-    padding-top: 45px;
-    margin: auto 10%;
-    @media screen and (max-width: 500px) {
-        margin: auto;
-        padding: 15px;
-    }
-`;
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 
 export default function Home() {
     const router = useRouter();
-    const [name, setName] = React.useState("");
+    const [name, setName] = React.useState('');
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -45,16 +36,17 @@ export default function Home() {
                     <Widget.Content>
                         {/* <p>{db.description}</p> */}
                         <form onSubmit={handleSubmit}>
-                            <input
+                            <Input
                                 type="text"
+                                name="nomeDoUsuario"
                                 value={name}
-                                onChange={(e) => setName(e.target.value)}
-                                placeholder="Seu nome.."
+                                onChange={e => setName(e.target.value)}
+                                placeholder="Seu nome..."
                             />
 
-                            <button type="submit" disabled={!name}>
-                                Jogar {name}
-                            </button>
+                            <Button type="submit" disabled={!name}>
+                                {`Jogar como: ${name}`}
+                            </Button>
                         </form>
                     </Widget.Content>
                 </Widget>
